d8301ad6468aeb0db0e43afeafafd4601f28238b
Rimon Akhter <>
2021-05-09 11:42:45 +0000

    comments added

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import discord
 import os
-import urbandictionary as ud
-
+import requests
+import json
+import urbandictionary as ud  
+                                          # urbandictionary repo link: https://github.com/bocong/urbandictionary-py
 from udpy import UrbanClient
 from discord.ext import commands
 
-client = commands.Bot(command_prefix='.')
-uClient = UrbanClient()
-token = os.environ['TOKEN']
+client = commands.Bot(command_prefix='.') # bot client, all commands start with '.'
+uClient = UrbanClient()                   # UD api wrapper client, https://pypi.org/project/udpy/
+token = os.environ['TOKEN']               # replit env variable access system, go to secrests for more info
 
-@client.event
-async def on_ready():
+@client.event                             # decorator for events
+async def on_ready():                     # refer to discord api. on_ready used for confirmation of bot coming online
     print('We have logged in as {0.user}'.format(client))
+                                          # print on console
 
 '''
 @client.event
@@ -24,8 +27,8 @@ async def on_message(message):
 '''
 
 
-@client.command()
-async def randomword(random):
+@client.command()                         # decorator for commands
+async def randomword(random):             # function name will be used as commands along with bot clientprefix
     rand = ud.random()
     for w in rand[:3]:
         await random.send(w.word)
@@ -35,7 +38,7 @@ async def randomword(random):
 @client.command()
 async def define(ctx, word):
     defs = uClient.get_definition(word)
-    for d in defs:
+    for d in defs[:2]:
         await ctx.send(d.definition)
 
 
